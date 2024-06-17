a50bb2a32e151a7ff41a51ac017d2bb5001d9880
vincentpchevalier <vincentpchevalier@proton.me>
2024-06-17 16:33:36 -0400

    build init and showToTop function for bottom skip link

--- js/index.js
+++ js/index.js
@@ -1,1 +1,32 @@
-console.log('Page loaded');
+let main;
+let toTop;
+
+function init() {
+	main = document.querySelector('main');
+	toTop = document.querySelector('.to-top');
+
+	let page = main.id.split('-')[1];
+
+	const mainObserver = new IntersectionObserver(showToTop, {
+		root: null,
+		rootMargin: '50px',
+		threshold: `${page === 'home' ? 0.5 : 0.45}`,
+	});
+
+	mainObserver.observe(main);
+}
+
+function showToTop(entries) {
+	// console.log('to top function');
+	const [entry] = entries;
+	if (!entry.isIntersecting) {
+		toTop.classList.remove('visible');
+		return;
+	}
+
+	toTop.classList.add('visible');
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+	init();
+});
