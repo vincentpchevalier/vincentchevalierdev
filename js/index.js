433bfbead5cdd837fd718d1b7551332d76f4cd5d
Vincent Pierre Chevalier <132299042+vincentpchevalier@users.noreply.github.com>
2024-06-17 22:03:45 -0400

    main page styling
    * complete basic markup for home page
    
    * import profile image
    
    * import photos from pexels for placeholder content
    
    * add README with photo credits and info about the site
    
    * add skip links and accessible navigation
    
    * write projects.html markup
    
    * create header / footer file for templating
    
    * add default styles
    
    * change wrapper class name to container
    
    * complete header and footer styling with skip links
    
    * reorder container locations
    
    * set up styling for intro section
    
    * complete block layout for sections and containers
    
    * add container to the header
    
    * design form and skills section
    
    * add pointer cursor to buttons
    
    * implement basic light/dark theme switch

--- js/index.js
+++ js/index.js
@@ -1,9 +1,20 @@
 let main;
 let toTop;
+let button;
 
 function init() {
+	let isDark =
+		document.documentElement.dataset.theme === 'dark' ||
+		matchMedia('(prefers-color-scheme: dark)').matches;
 	let isHome =
 		location.pathname.includes('index.html') || location.pathname === '/';
+
+	button = document.querySelector('.toggle');
+
+	button.setAttribute('aria-pressed', isDark ? false : true);
+	document.documentElement.dataset.theme = isDark ? 'dark' : 'light';
+
+	button.addEventListener('click', sync);
 
 	main = isHome
 		? document.querySelector('#home #main')
@@ -18,6 +29,16 @@ function init() {
 
 	mainObserver.observe(main);
 }
+
+const sync = () => {
+	const darkNow = button.matches('[aria-pressed=false]');
+	console.log(`was ${document.documentElement.dataset.theme}`);
+	document.documentElement.dataset.theme = darkNow ? 'light' : 'dark';
+	console.log(`now ${document.documentElement.dataset.theme}`);
+	console.log(`Now with ${darkNow ? 'Dark' : 'Light'} Mode.`);
+	button.setAttribute('aria-pressed', darkNow ? true : false);
+	button.textContent = darkNow ? 'DM' : 'LM';
+};
 
 function showToTop(entries) {
 	// console.log('to top function');
